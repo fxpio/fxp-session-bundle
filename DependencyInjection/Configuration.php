8f237a6a5ab01117de41030f5f09b3b2efcd628b
François Pluchino <francois.pluchino@gmail.com>
2019-05-14 12:45:06 +0200

    Remove access to the php super globals

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -100,6 +100,6 @@ class Configuration implements ConfigurationInterface
 
     private function hasEnvUrl(): bool
     {
-        return isset($_ENV['DATABASE_URL']) || isset($_SERVER['DATABASE_URL']) || false !== getenv('DATABASE_URL');
+        return false !== getenv('DATABASE_URL');
     }
 }
